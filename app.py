526a6ec89deda9eeee043d0c39b78158e0f07dc5
tkucd <92442298+tkucd@users.noreply.github.com>
2023-07-17 23:42:44 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# テスト
+@app.get('/')
+def test():
+    return {'Hello': 'World!!!!'}
