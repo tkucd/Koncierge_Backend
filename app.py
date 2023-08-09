45d93513e38392fdfb453101271213e481340fc2
tkucd <92442298+tkucd@users.noreply.github.com>
2023-08-09 14:30:43 +0900

    update

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
+from make import make_cuisine
+
 app = FastAPI()
 
 app.add_middleware(
@@ -12,7 +14,22 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# 料理を作る際に送られる情報
+class Info(BaseModel):
+    ingredients: str
+    kcal: int
+    time: int
+
 # テスト
 @app.get('/')
 def test():
     return {'Hello': 'World!!!!'}
+
+# 与えられた情報から料理を作成
+@app.post('/make')
+def return_recipe(info: Info):
+    return make_cuisine(
+        info.ingredients, 
+        info.kcal, 
+        info.time
+    )
